1169211e19984870db65962c96a0507403690d8f
Gordxn <ashtonivy@icloud.com>
2019-10-20 09:16:10 -0700

    Added canManage; Added comments to help people out

--- api/user.py
+++ api/user.py
@@ -8,6 +8,12 @@ class User:
     def UsernameById(id):
         return requests.get(url='http://api.roblox.com/users/' + str(id)).json()["Username"]
 
+    #/users/get-by-username?username={username}
     @staticmethod
     def IdByUsername(username):
         return requests.get(url='http://api.roblox.com/users/get-by-username?username=' + str(username)).json()["Id"]
+
+    #/users/{id}/canmanage/{assetId}
+    @staticmethod
+    def canManage(id, assetId):
+        return requests.get(url='http://api.roblox.com/users/' + str(id) + '/canmanage/' + str(assetId)).json()["CanManage"]
