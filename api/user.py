05e4e53214696fd7a3913891ab4910d32a220fd8
Gordxn <ashtonivy@icloud.com>
2019-10-19 19:33:26 -0700

    Added new method 'IdByUsername'
    Added new method 'IdByUsername'
    Also made it so it returns the Username or Id in this class instead of having the user pick it out from the JSON code.

--- api/user.py
+++ api/user.py
@@ -6,4 +6,8 @@ class User:
      #/users/{id}
     @staticmethod
     def UsernameById(id):
-        return requests.get(url='http://api.roblox.com/users/' + str(id)).json()
+        return requests.get(url='http://api.roblox.com/users/' + str(id)).json()["Username"]
+
+    @staticmethod
+    def IdByUsername(username):
+        return requests.get(url='http://api.roblox.com/users/get-by-username?username=' + str(username)).json()["Id"]
