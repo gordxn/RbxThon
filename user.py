0bc3a84656713ec8baf83345a853f85932625321
Gordxn <ashtonivy@icloud.com>
2019-10-19 17:00:19 -0700

    Delete user.py

--- user.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-import json
-
-class User:
-     #/users/{id}
-    @staticmethod
-    def UsernameById(self, id):
-        return requests.get(url='http://api.roblox.com/users/' + str(id))
