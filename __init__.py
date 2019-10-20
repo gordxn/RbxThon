60c6f6c2ae543264d3def7cb83080f9ab10ed536
Gordxn <ashtonivy@icloud.com>
2019-10-19 17:58:18 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,16 +1,2 @@
-#
-#  __init__.py
-#  RyThon
-#
-#  By Gordxn
-#  Copyright © 2019 Gordxn. All rights reserved.
-#
-
-__title__ = 'RyThon'
-__author__ = 'Gordxn'
-__license__ = 'MIT'
-__copyright__ = 'Copyright 2019 Gordxn'
-__version__ = '0.0.1'
-
 #Parent Class Modules
 from .api.user import User
